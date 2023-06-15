deb7cb1d78ba237e4518a00bed44c726f796bf5b
Andreea-Samoila <108801325+Andreea-Samoila@users.noreply.github.com>
2023-06-15 11:53:54 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,11 +1,13 @@
 import convertor.EnglishNumberToWords;
 import convertor.RomanianNumberToWords;
 
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println(EnglishNumberToWords.convertNumberToWords("7004343000"));
-        System.out.println(RomanianNumberToWords.convertNumberToWords("103400234"));
+        System.out.println(RomanianNumberToWords.convertNumberToWords("10323223243"));
 
     }
 
